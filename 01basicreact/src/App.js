8d9a4d30dce16625603900f5347ed71bb2d2e817
vishnugajmal <vishnugajmal89456@gmail.com>
2024-05-27 18:16:09 +0530

    Add basic component

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello </h1>
+      <h1>Hello Vishnu </h1>
     </div>
   );
 }
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,5 @@
+function Chai(){
+    <h1>Hello i am learning react with chai aur code</h1>
+
+}
+export default Chai;
