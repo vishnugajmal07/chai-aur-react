47aebc3646e6225c08da7c50694b7da9dfd4d941
vishnugajmal <vishnugajmal89456@gmail.com>
2024-05-27 18:25:06 +0530

    Basic Changes

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Chai from "./Chai";
 
 function App() {
 let [counter, setCounter]=useState(10);
@@ -13,10 +14,12 @@ const removeNumber =()=>{
 }
   return (
     <>
-        <h1>CC with VG</h1>
+        <h1>CC with Vishnu</h1>
         <h1>Value {counter}</h1>
         <button onClick={addNumber}>Add </button>
         <button onClick={removeNumber}>Remove</button>
+
+        <Chai/>
     </>
   )
 }
